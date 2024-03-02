bf34531a542e9122dc9ef26674ac1ad39860d5ad
Mohammed Afif <118624877+Mohammed-Afif8951@users.noreply.github.com>
2024-03-02 19:36:18 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
@@ -14,4 +14,17 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+//routes import 
+
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+
+
 export { app }
--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js";
 
-const connectDB = async ()=>{
+
+const connectDB = async () =>{
     try {
-        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`MONGODB connected !! Db HOST: `)
+      const connectInstance =  await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MONGODB connected !! Db HOST: ${connectInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection Failed",error)
         process.exit(1);
